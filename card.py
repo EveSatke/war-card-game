189a8a8387b298ad6fd0d156f90ebcb906e25aef
ginsta6 <gintazzz9@gmail.com>
2024-11-28 16:11:08 +0200

    Game round implementation

--- card.py
+++ card.py
@@ -1,16 +1,30 @@
 class Card:
 
-    RANKS = {"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":11, "Q":12, "K":12, "A":13}
-    
+    RANKS = {
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 10,
+        "J": 11,
+        "Q": 12,
+        "K": 12,
+        "A": 13,
+    }
+
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
-    def compare_value(self):
-        self.rank 
+    def compare_value(self, other: "Card"):
+        return Card.RANKS[self.rank] - Card.RANKS[other.rank]
 
     def __str__(self):
         return f"{self.rank} of {self.suit}"
-    
+
     def __repr__(self):
         return f"Rank= {self.rank} Suit= {self.suit}"
